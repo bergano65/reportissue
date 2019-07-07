cb02473c3ab96ac7ca17078feeb60d5406d74c4c
Evgeny Vitenberg <eviten@microsoft.com>
2019-07-07 22:27:27 +0300

    add/remov picture  draw markers

--- ReportIssue/EditIssueDlg2.xaml.cs
+++ ReportIssue/EditIssueDlg2.xaml.cs
@@ -22,27 +22,14 @@ namespace ReportIssue
 {
     public partial class EditIssueDlg2 : Window
     {
-        private bool _isImgDrawn;
         private bool _isIssueEdited;
-        private bool _isMarkerDrawn;
-        private IOperationHolder<RequestTelemetry> _markerOp;
-        private System.Windows.Point _markerStartPosition;
-        private System.Windows.Shapes.Rectangle _markerRect;
         private TelemetryClient _tc;
         private Window _parentWindow;
         private WindowState _parentWindowState;
         private WindowState _windowState;
         public Issue CurrentIssue;
-        private System.Windows.Controls.Image _img;
-        private System.Windows.Forms.Timer _timer;
-
-
-        public List<System.Drawing.Rectangle> Markers { get; set; }
 
         public bool Saved { get; set; }
-
-        [DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
 
         public EditIssueDlg2(Window parent, Issue issue)
         {
@@ -50,75 +37,14 @@ namespace ReportIssue
             this._tc.TrackPageView("Edit Issue");
             this._tc.TrackEvent("Start edit issue", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
             this._parentWindow = parent;
-            this.Markers = new List<System.Drawing.Rectangle>();
             this.InitializeComponent();
             this.CurrentIssue = issue;
-            this._timer = new System.Windows.Forms.Timer();
-            this._timer.Interval = 50;
-            this._timer.Tick += new EventHandler(this._timer_Tick);
-            this._timer.Start();
-            this._isMarkerDrawn = false;
-        }
-
-        private void _timer_Tick(object sender, EventArgs e)
-        {
-            if (!this._isImgDrawn)
-                return;
-            if (Mouse.LeftButton == MouseButtonState.Pressed)
-            {
-                if (!this._isMarkerDrawn)
-                {
-                    System.Windows.Point p  = Mouse.GetPosition((IInputElement)this._img);
-                    if (p.X >= 0 && p.X <= this._img.Width && p.Y >= 0 && p.Y <= this._img.Height) 
-                    {
-                        return;
-                    }
-
-                    this._tc.TrackEvent("Start marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
-                    this._markerOp = this._tc.StartOperation<RequestTelemetry>(new RequestTelemetry("Place marker", (DateTimeOffset)DateTime.Now, new TimeSpan(0, 0, 40), "200", true));
-                    this._markerStartPosition = Mouse.GetPosition((IInputElement)this._img);
-                    this._markerRect = new System.Windows.Shapes.Rectangle();
-                    this._markerRect.Stroke = (System.Windows.Media.Brush)new SolidColorBrush(Colors.Red);
-                    this._markerRect.StrokeThickness = 2.0;
-                    Canvas.SetLeft((UIElement)this._markerRect, this._markerStartPosition.X);
-                    Canvas.SetTop((UIElement)this._markerRect, this._markerStartPosition.Y);
-                    this._imgCanvas.Children.Add((UIElement)this._markerRect);
-                    this._isMarkerDrawn = true;
-                }
-                else
-                    this.DrawMarker(Mouse.GetPosition((IInputElement)this._img));
-            }
-            else
-            {
-                if (!this._isMarkerDrawn)
-                    return;
-                this._tc.TrackEvent("Finish marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
-                this._tc.StopOperation<RequestTelemetry>(this._markerOp);
-                Rect markerRect = this.GetMarkerRect(this._markerStartPosition, Mouse.GetPosition((IInputElement)this._img));
-                this.Markers.Add(new System.Drawing.Rectangle((int)markerRect.X, (int)markerRect.Y, (int)markerRect.Width, (int)markerRect.Height));
-                this._markerRect = (System.Windows.Shapes.Rectangle)null;
-                this._isIssueEdited = true;
-                this._isMarkerDrawn = false;
-            }
-        }
-
-        private Rect GetMarkerRect(System.Windows.Point markerStartPosition, System.Windows.Point mousePosition)
-        {
-            return new Rect()
-            {
-                X = Math.Min(markerStartPosition.X, mousePosition.X),
-                Y = Math.Min(markerStartPosition.Y, mousePosition.Y),
-                Width = Math.Abs(markerStartPosition.X - mousePosition.X),
-                Height = Math.Abs(markerStartPosition.Y - mousePosition.Y)
-            };
         }
 
         private void NewIssue()
         {
             this._tc.TrackEvent("New Issue", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
-            this._imgCanvas.Children.Clear();
             this._isIssueEdited = true;
-            this._isMarkerDrawn = false;
             this.Saved = false;
         }
 
@@ -132,13 +58,48 @@ namespace ReportIssue
             return this._issuePropsTabControl.CheckIssueProperties();
         }
 
+        private void removePictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_pictureTabControl.SelectedIndex != -1)
+            {
+                _pictureTabControl.Items.RemoveAt(_pictureTabControl.SelectedIndex);
+            }
+
+            for (int i = 0; i < _pictureTabControl.Items.Count; i++)
+            {
+                (_pictureTabControl.Items[i] as TabItem).Header = string.Format("Picture {0}", i + 1);
+            }
+        }
+
+        private void addPictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem item = new TabItem();
+            item.Content = new ShotPanel();
+            item.Header = string.Format("Picture {0}", _pictureTabControl.Items.Count + 1);
+            _pictureTabControl.Items.Add(item);
+            _pictureTabControl.SelectedIndex = _pictureTabControl.Items.Count - 1;
+        }
+
         private void shotButton_Click(object sender, RoutedEventArgs e)
         {
+            if (_pictureTabControl.Items.Count == 0 || 
+                (_pictureTabControl.SelectedIndex == -1 && _pictureTabControl.Items.Count > 1))
+            { 
+                return;
+            }
+           
             this._tc.TrackEvent("Take Screenshot", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
+            int selIndex =
+                _pictureTabControl.SelectedIndex != -1 ?
+                _pictureTabControl.SelectedIndex : 0;
+
+            ShotPanel shotPanel = (_pictureTabControl.Items[selIndex] as TabItem).Content as ShotPanel;
+
             this._windowState = this.WindowState;
             this._parentWindowState = this._parentWindow.WindowState;
             this.WindowState = WindowState.Minimized;
             this._parentWindow.WindowState = WindowState.Minimized;
+
             System.Drawing.Rectangle bounds1 = Screen.PrimaryScreen.Bounds;
             int width1 = bounds1.Width;
             bounds1 = Screen.PrimaryScreen.Bounds;
@@ -152,47 +113,33 @@ namespace ReportIssue
             bounds2 = Screen.PrimaryScreen.Bounds;
             System.Drawing.Size size = bounds2.Size;
             graphics.CopyFromScreen(x, y, 0, 0, size, CopyPixelOperation.SourceCopy);
-            IntPtr hbitmap = bitmap.GetHbitmap();
-            IntPtr palette = (IntPtr)0;
-            Int32Rect sourceRect = new Int32Rect(0, 0, bitmap.Width, bitmap.Height);
-            System.Windows.Size renderSize = this._imgCanvas.RenderSize;
-            int width2 = (int)renderSize.Width;
-            renderSize = this._imgCanvas.RenderSize;
-            int height2 = (int)renderSize.Height;
-            BitmapSizeOptions sizeOptions = BitmapSizeOptions.FromWidthAndHeight(width2, height2);
-            BitmapSource sourceFromHbitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, palette, sourceRect, sizeOptions);
-            this._img = new System.Windows.Controls.Image();
-            this._img.Source = (ImageSource)sourceFromHbitmap;
-            this._isImgDrawn = true;
-            if (this._imgCanvas.Children.Count > 0 && this._imgCanvas.Children[0] is System.Windows.Controls.Image)
-                this._imgCanvas.Children.RemoveAt(0);
-            this._imgCanvas.Children.Insert(0, (UIElement)this._img);
-            Canvas.SetLeft((UIElement)this._img, 0.0);
-            Canvas.SetRight((UIElement)this._img, 0.0);
-            EditIssueDlg2.DeleteObject(hbitmap);
-            this._isImgDrawn = true;
-            this.CurrentIssue.Picture = bitmap;
+
+            shotPanel.SetPicture(bitmap);
+
             this._parentWindow.WindowState = this._parentWindowState;
             this.WindowState = this._windowState;
-        }
+
+    }
 
         private void removeMarkButton_Click(object sender, RoutedEventArgs e)
         {
-            this._tc.TrackEvent("Remove markers", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
-            if (!this._isImgDrawn)
-                return;
-            this.Markers.Clear();
-            for (int index = this._imgCanvas.Children.Count - 1; index > 0; --index)
-                this._imgCanvas.Children.RemoveAt(index);
-            this._isIssueEdited = true;
+            /*
+                        this._tc.TrackEvent("Remove markers", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
+                        if (!this._isImgDrawn)
+                            return;
+                        this.Markers.Clear();
+                        for (int index = this._imgCanvas.Children.Count - 1; index > 0; --index)
+                            this._imgCanvas.Children.RemoveAt(index);
+                        this._isIssueEdited = true;
+            */
         }
 
         private void ShowIssue()
         {
+            /*
             this._tc.TrackEvent("Show Issue", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
             this._isIssueEdited = false;
             this.CurrentIssue.Open();
-            this._imgCanvas.Children.Clear();
             Bitmap picture = this.CurrentIssue.Picture;
             IntPtr hbitmap = picture.GetHbitmap();
             IntPtr palette = (IntPtr)0;
@@ -228,6 +175,7 @@ namespace ReportIssue
                 this._imgCanvas.Children.Add((UIElement)rectangle2);
             }
             this._issuePropsTabControl.GetIssueProperties(this.CurrentIssue);
+            */
         }
 
         private void SelectComboBoxItem(System.Windows.Controls.ComboBox comboBox, string value)
@@ -245,6 +193,8 @@ namespace ReportIssue
 
         private bool SaveIssue()
         {
+            return true;
+            /*
             this._tc.TrackEvent("Save Issue", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
             if (!this.CheckCurrentIssueProperties())
             {
@@ -280,16 +230,7 @@ namespace ReportIssue
             this._isMarkerDrawn = false;
             this._isIssueEdited = false;
             return true;
-        }
-
-        private void DrawMarker(System.Windows.Point mousePosition)
-        {
-            this._tc.TrackEvent("Draw Marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
-            Rect markerRect = this.GetMarkerRect(this._markerStartPosition, mousePosition);
-            Canvas.SetLeft((UIElement)this._markerRect, markerRect.X);
-            Canvas.SetTop((UIElement)this._markerRect, markerRect.Y);
-            this._markerRect.Width = markerRect.Width;
-            this._markerRect.Height = markerRect.Height;
+            */
         }
 
         private void _okBtn_Click(object sender, RoutedEventArgs e)
---
+++ ReportIssue/ShotPanel.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace ReportIssue
+{
+    /// <summary>
+    /// Interaction logic for ShotPanel.xaml
+    /// </summary>
+    public partial class ShotPanel : UserControl
+    {
+        private bool _isImgDrawn;
+        private bool _isMarkerDrawn;
+        private TelemetryClient _tc;
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        private System.Windows.Controls.Image _img;
+        private System.Windows.Forms.Timer _timer;
+        public List<System.Drawing.Rectangle> Markers { get; set; }
+
+        private IOperationHolder<RequestTelemetry> _markerOp;
+        private System.Windows.Point _markerStartPosition;
+        private System.Windows.Shapes.Rectangle _markerRect;
+
+        public ShotPanel()
+        {
+            InitializeComponent();
+            this._tc = new TelemetryClient();
+
+            this.Markers = new List<System.Drawing.Rectangle>();
+            this._timer = new System.Windows.Forms.Timer();
+            this._timer.Interval = 50;
+            this._timer.Tick += new EventHandler(this._timer_Tick);
+            this._timer.Start();
+            this._isMarkerDrawn = false;
+
+            this.ImgCanvas.Children.Clear();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            if (!this._isImgDrawn)
+                return;
+            if (!this._isMarkerDrawn)
+                return;
+            this.DrawMarker(Mouse.GetPosition((IInputElement)this._img));
+        }
+
+       private void DrawMarker(System.Windows.Point mousePosition)
+        {
+            this._tc.TrackEvent("Draw Marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
+            Rect markerRect = this.GetMarkerRect(this._markerStartPosition, mousePosition);
+            Canvas.SetLeft((UIElement)this._markerRect, markerRect.X);
+            Canvas.SetTop((UIElement)this._markerRect, markerRect.Y);
+            this._markerRect.Width = markerRect.Width;
+            this._markerRect.Height = markerRect.Height;
+        }
+
+        public void SetPicture(Bitmap bitmap)
+        {
+            IntPtr hbitmap = bitmap.GetHbitmap();
+            IntPtr palette = (IntPtr)0;
+            Int32Rect sourceRect = new Int32Rect(0, 0, bitmap.Width, bitmap.Height);
+//            BitmapSizeOptions sizeOptions = BitmapSizeOptions.FromWidthAndHeight(bitmap.Width, bitmap.Height);
+
+             System.Windows.Size renderSize = ImgCanvas.RenderSize;
+                int width2 = (int)renderSize.Width;
+                int height2 = (int)renderSize.Height;
+                BitmapSizeOptions sizeOptions = BitmapSizeOptions.FromWidthAndHeight(width2, height2);
+                BitmapSource sourceFromHbitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, palette, sourceRect, sizeOptions);
+
+
+            this._img = new System.Windows.Controls.Image();
+            this._img.Source = (ImageSource)sourceFromHbitmap;
+
+//            this._img.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, palette, sourceRect, sizeOptions);
+//            this._img.Stretch = Stretch.Fill;
+
+            this._isImgDrawn = true;
+
+            if (this.ImgCanvas.Children.Count > 0 && this.ImgCanvas.Children[0] is System.Windows.Controls.Image)
+            {
+                this.ImgCanvas.Children.RemoveAt(0);
+            }
+
+            this._img.MouseDown += _img_MouseDown;
+            this._img.MouseUp += _img_MouseUp;
+
+            this.ImgCanvas.Children.Insert(0, (UIElement)this._img);
+            Canvas.SetLeft((UIElement)this._img, 0.0);
+            Canvas.SetRight((UIElement)this._img, 0.0);
+            DeleteObject(hbitmap);
+            this._isImgDrawn = true;
+        }
+
+        private void _img_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!this._isImgDrawn)
+                return;
+            if (!this._isMarkerDrawn)
+                return;
+
+            this._tc.TrackEvent("Finish marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
+            this._tc.StopOperation<RequestTelemetry>(this._markerOp);
+            Rect markerRect = this.GetMarkerRect(this._markerStartPosition, Mouse.GetPosition((IInputElement)this._img));
+            this.Markers.Add(new System.Drawing.Rectangle((int)markerRect.X, (int)markerRect.Y, (int)markerRect.Width, (int)markerRect.Height));
+            this._markerRect = (System.Windows.Shapes.Rectangle)null;
+            this._isMarkerDrawn = false;
+        }
+
+        private void _img_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!this._isImgDrawn)
+                return;
+            if (this._isMarkerDrawn)
+            {
+                return;
+            }
+
+            this._tc.TrackEvent("Start marker", (IDictionary<string, string>)null, (IDictionary<string, double>)null);
+            this._markerOp = this._tc.StartOperation<RequestTelemetry>(new RequestTelemetry("Place marker", (DateTimeOffset)DateTime.Now, new TimeSpan(0, 0, 40), "200", true));
+            this._markerStartPosition = Mouse.GetPosition((IInputElement)this._img);
+            this._markerRect = new System.Windows.Shapes.Rectangle();
+            this._markerRect.Stroke = (System.Windows.Media.Brush)new SolidColorBrush(Colors.Red);
+            this._markerRect.StrokeThickness = 2.0;
+            Canvas.SetLeft((UIElement)this._markerRect, this._markerStartPosition.X);
+            Canvas.SetTop((UIElement)this._markerRect, this._markerStartPosition.Y);
+            this.ImgCanvas.Children.Add((UIElement)this._markerRect);
+            this._isMarkerDrawn = true;
+         }
+
+            private Rect GetMarkerRect(System.Windows.Point markerStartPosition, System.Windows.Point mousePosition)
+        {
+            return new Rect()
+            {
+                X = Math.Min(markerStartPosition.X, mousePosition.X),
+                Y = Math.Min(markerStartPosition.Y, mousePosition.Y),
+                Width = Math.Abs(markerStartPosition.X - mousePosition.X),
+                Height = Math.Abs(markerStartPosition.Y - mousePosition.Y)
+            };
+        }
+
+        public void RemoveMarkers()
+        {
+
+        }
+    }
+}
